fe1eb157f4f8066e39ce78a6d2db54274a82290b
Lucas Jacinto <silval@alunos.utfpr.edu.br>
2018-04-17 00:35:33 -0300

    mudando mensagem de erro

--- hello-world-test/index.js
+++ hello-world-test/index.js
@@ -10,7 +10,7 @@ module.exports = function (context, req) {
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Ops! Algo deu errado: Não se esqueça de enviar seu nome!"
         };
     }
     context.done();
